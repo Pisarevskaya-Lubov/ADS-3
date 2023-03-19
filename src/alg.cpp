ddd2d2f1a0f0ccae97eb03ebee6ab12394c3ee76
Pisarevskaya-Lubov <125238314+Pisarevskaya-Lubov@users.noreply.github.com>
2023-03-19 12:37:09 +0300

    b123

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,45 +1,47 @@
 // Copyright 2021 NNTU-CS
 
 void bubblSort(int* arr, int size) {
-	int basket = 0;
-	for (int j = 0; j < size - 1; j++) {
-		for (int i = 0; i < size - j - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				basket = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = basket;
-			}
-		}
-	}
-
+  int basket = 0;
+  for (int j = 0; j < size - 1; j++) {
+    for (int i = 0; i < size - j - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        basket = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = basket;
+      }
+    }
+  }
 }
 
 int cbinsearch(int* arr, int size, int value) {
-	int res = 0;
-	int number = 0;
-	int left = 0;
-	int right = size - 1;
-
-	while (left <= right) {
-		int midl = left + (right - left) / 2;
-		if (value == arr[midl]) {
-			number = midl;
-			break;
-		}
-		else if (value > arr[midl]) {
-			left = midl;
-		}
-		else if (value < arr[midl]) {
-			right = midl;
-		}
-		else {
-			return 1000;
-		}
-	}
-	while (arr[number] == value) {
-		res++;
-		number++;
-	}
-	return res;
+  int res = 0;
+  int number = 0;
+  int left = 0;
+  int right = size - 1;
+  while (left <= right) {
+    int midl = left + (right - left) / 2;
+    if (value == arr[midl]) {
+      number = midl;
+      break;
+    } else if (value > arr[midl]) {
+      left = midl;
+    } else if (value < arr[midl]) {
+      right = midl;
+    } else {
+      return 1000;
+    }
+  }
+  n = number;
+  while (arr[n] == value) {
+    res++;
+    n++;
+  }
+  if (number > 0) {
+    while(arr[number-1]==value){
+      res++;
+      number--;
+    }
+  }
+  return res;
 }
 
