8cf80a1827760916bbdf3b59d93e3c735a685129
Pisarevskaya-Lubov <125238314+Pisarevskaya-Lubov@users.noreply.github.com>
2023-03-19 16:32:44 +0300

    b123

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,5 @@
 // Copyright 2021 NNTU-CS
 
-void bubblSort(int* arr, int size) {
-  int basket = 0;
-  for (int j = 0; j < size - 1; j++) {
-    for (int i = 0; i < size - j - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        basket = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = basket;
-      }
-    }
-  }
-}
-
 int cbinsearch(int* arr, int size, int value) {
   int res = 0;
   int number = 0;
