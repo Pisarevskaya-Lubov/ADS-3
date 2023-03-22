a72d545ca19be57be13b0d82d9d4d554e2830b67
Pisarevskaya-Lubov <125238314+Pisarevskaya-Lubov@users.noreply.github.com>
2023-03-22 19:01:18 +0300

    b123

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,35 +1,36 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int* arr, int size, int value) {
-    int res = 0;
-    int size2 = size / 2;
-    int size3 = 0;
-    int number = 0;
-    int size4 = 0;
-    while (size2 - size3 > 0) {
-        size4 = size2 - size3 / 2;
-        if (arr[size4] > value) {
-            size2 = size4 - 1;
-        }
-        else if (arr[size4] < value) {
-            size3 = size4 + 1;
-        }
-        else {
-            res++;
-            number = size4 - 1;
-            while (arr[number] == value) {
-                res++;
-                number++;
-            }
-            number = size4 + 1;
-            if (number > 0) {
-                while (arr[number - 1] == value) {
-                    res++;
-                    number--;
-                }
-            }
-        }
-        return res;
-    }
+	int res = 0;
+	int size2 = size / 2;
+	int size3 = 0;
+	int size4 = 0;
+	while (size2 - size3 > 0) {
+		size4 = size2 - size3 / 2;
+		if (arr[size4] > value) {
+			size2 = size4 - 1;
+		} else if (arr[size4] < value) {
+			size3 = size4 + 1;
+		} else if (arr[size4] == value) {
+			res++;
+			int number = size4 - 1;
+			while (arr[number] == value) {
+				res++;
+				number++;
+			}
+			number = size4 + 1;
+			if (number > 0) {
+				while (arr[number - 1] == value) {
+					res++;
+					number--;
+				}
+			}
+
+		} else {
+			return 0;
+		}
+		return res;
+	}
+	return 0;
 }
 
